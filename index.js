97b04cf7fdaf4d6d550be4fad1d05efd1c44cd85
kubat <kubat.aiupov@gmail.com>
2022-12-20 21:40:53 +0100

    init random user generator

---
+++ index.js
@@ -0,0 +1,46 @@
+const p = document.querySelector('#para');
+const btn = document.querySelector('#generate-btn')
+
+const usersNames = ['Amir', 'Ivan', 'Jorg', 'Kevin', 'John', 'Mustafa', 'Adrian'];
+const usersLastNames = ['Doe', 'Boe', 'Coe', 'Koe', 'Joe','Hoe', 'Loe', 'Goe', 'Poe'];
+
+// generates a random number for userId
+const generateUserID = () => Math.floor(Math.random() * 200);
+
+// generates a ranodm user first name
+const generateFirstName = () => { 
+  return usersNames[Math.floor(Math.random() * usersNames.length)]
+}
+
+// generates a random user last name
+const generateLastName = () => { 
+  return usersLastNames[Math.floor(Math.random() * usersLastNames.length)]
+}
+
+// generates a user email
+const generateUserEmail = (fName, lName) => {
+  fName = generateFirstName();
+  lName = generateLastName();
+  let email = `${fName.toLowerCase()}.${lName.toLowerCase()}@mail.com`
+  return email;
+}
+
+generateFirstName();
+generateLastName();
+generateUserID();
+//console.log(generateUserID());
+//p.innerHTML = generateUserEmail();
+
+// generates a random User and displays it to the screen
+generateRandomUser = () => {};
+
+// display user info
+const displayUser = (e) => {
+  e.preventDefault();
+  p.innerHTML = `user_name: ${generateFirstName()}*** 
+  user_last_name: ${generateLastName()}***
+  user_id: ${generateUserID()}***
+   user_email: ${generateUserEmail()}***`
+}
+
+btn.addEventListener('click', displayUser);
